07c97378b5d11a424f0298ce15c808e8b90001bd
David Keimig <davidkeimig@gmail.com>
2020-08-18 21:39:22 -0400

    Initial Commit

---
+++ Boast/BoastApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoastApp.swift
+//  Boast
+//
+//  Created by David Keimig on 8/18/20.
+//
+
+import SwiftUI
+
+@main
+struct BoastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Boast
+//
+//  Created by David Keimig on 8/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
